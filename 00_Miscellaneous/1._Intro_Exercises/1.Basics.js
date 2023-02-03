9aa9c20cf912295ac4a24b58920b0ef0f58dfc4d
Violetta87 <athena_mewo87@hotmail.com>
2023-02-03 12:14:08 +0100

    basic exercies added

---
+++ 00_Miscellaneous/1._Intro_Exercises/1.Basics.js
@@ -0,0 +1,33 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+console.log("My first name is: " + firstName + " and my lastname is: " + lastName)
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2021";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2020
+// You cannot touch line 1 or 2
+
+const result = parseInt(year) + number
+const resultTwo = +yeay + number;
+
+console.log(resultTwo)
+
+console.log(result)
+
+
+
+// --------------------------------------
