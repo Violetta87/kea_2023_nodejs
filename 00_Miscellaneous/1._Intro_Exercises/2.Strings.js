853be968353eb47aaa769b464edf83e857a449d9
Violetta87 <athena_mewo87@hotmail.com>
2023-02-03 13:51:51 +0100

    finished 2strings exercise

---
+++ 00_Miscellaneous/1._Intro_Exercises/2.Strings.js
@@ -0,0 +1,59 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const result = parseFloat("1.10");
+const resultTwo = parseFloat("2.30");
+
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+const resultThree = parseFloat("1.10") + parseFloat("2.30")
+const resultThreeWithTWoDecimals = resultThree.toFixed(2);
+
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+const averageResult = (one + two + three)/3;
+const averageWithDecimals = averageResult.toFixed(5);
+
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+const resultFour = letters.charAt(2);
+
+
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+const resultCapitalJ = fact.replace("j", "J");
+
+// --------------------------------------
+
+
+
