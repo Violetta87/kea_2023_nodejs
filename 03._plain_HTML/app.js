7d5f18f8ad3d530189cf470661a8e4238b82020d
Violetta87 <athena_mewo87@hotmail.com>
2023-02-17 11:50:35 +0100

    publi and tanks folder added

--- 03._plain_HTML/app.js
+++ 03._plain_HTML/app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 
+//vi sætter public som vores static folder - klienten må gerne tage filer. 
+//klient skal aldrig få adgang til serverside. 
+app.use(express.static("public"));
+
+
+
 console.log(__dirname)//giver ikke et slash
 
 //vi creater en route
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/frontpage.html");
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
 });
+
+app.get("/tanks", (req, res) =>{
+    res.sendFile(__dirname + "/public/tanks/tanks.html")
+});
+
+
 
 //caps - fordi den er const og skal ikke ændres
 //listen function tager parametre error, og derved kan vi håndtere errors.
