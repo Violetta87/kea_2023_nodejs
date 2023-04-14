f03853fd86e70602023c22b44ebf02374421a96f
Violetta87 <athena_mewo87@hotmail.com>
2023-04-14 11:51:53 +0200

    half of it works

--- 00_Miscellaneous/asynchronous.js
+++ 00_Miscellaneous/asynchronous.js
@@ -23,11 +23,65 @@ Two states:
 - fullfiled (når det er fullfilled; 
     - rejected 
     - resolved)
+
+Solution 3: 
+
 */ 
 
 new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("yaaaay")
+        try{
+            resolve("yaaayy")
+        }catch (errorMessage){
+            reject(errorMessage)
+        }
+       
     }, 3000)
-}).then(succesMesssage => console.log(succesMesssage))
+}).then(succesMesssage => console.log("succesMessage:", succesMesssage))
+.catch(errorMessage => console.log("Error message: ", errorMessage)); 
+
+//create a function called celebrate that returns a promise that celebrates or not
+
+//celebrate er wrapped i et promise. 
+async function celebrate(name){
+    return new Promise((resolve, reject) => {
+        if(name){
+            resolve("wuhuuuu")
+        }
+        else{
+            
+
+        }
+        
+    })
+}
+
+celebrate().then(celebrationMessage => console.log(celebrationMessage))
+
+function somethingGoodSomethingBad(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try{
+                resolve("good")
+            }catch{
+                reject("baaad")
+            }
+        }, 2000)
+       
+    })
+}
+
+//somethingGoodSomethingBad().then(console.log).catch(console.log)
+
+//ifis. - wrapper functions i et scope og invoker med det samme. 
+(async function getGoodOrBadMessage(){
+    const somethingGoodSomethingBadMessage = await somethingGoodSomethingBad();
+    const celebrationMessage = await celebrate("All of us")
+    console.log(somethingGoodSomethingBadMessage, celebrationMessage)
+})()
+
+function parallel(){
+    Promise.all([somethingGoodSomethingBad(), celebrate("all of us")])
+}
+
 
